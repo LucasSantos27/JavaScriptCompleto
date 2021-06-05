8711b1b9b08e762fa1e5ae482e15fc5dd8ac5c02
unknown <lucas-unifei@outlook.com>
2021-06-05 00:23:58 -0300

    Iniciante - Variáveis

---
+++ javascript-para-iniciantes/variaveis/script-aula.js
@@ -0,0 +1,32 @@
+var nome = 'Lucas';
+var idade = 21;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Santos',
+    cidade = 'Itajuba';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefine);
+
+var comida;
+console.log(comida);
+comida = 'Pizza';
+console.log(comida);
+
+const time = 'Corinthians';
+time = 'Flamengo';
+
+console.log(time);
+
