2cef1578ed9aac43403a1c2257ee05cddcf58557
unknown <lucas-unifei@outlook.com>
2021-06-05 14:55:50 -0300

    Iniciante - Tipos de Dados

---
+++ javascript-para-iniciantes/tipos-de-dados/script-aula.js
@@ -0,0 +1,28 @@
+var idade = 21;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+var nome = 'Lucas';
+var sobrenome = 'Santos';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = 'Pelé fez ' + gols + ' gols';
+// console.log(typeof frase);
+
+var ano = '2021';
+var mes = 6;
+console.log(ano + mes);
+
+var melhor = 'teste';
+var frase1 = "Esse é o \"melhor\" jogo";
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Tulio Maravilha fez ${gols * 2} gols`;
+
+console.log(frase2);
+
+
